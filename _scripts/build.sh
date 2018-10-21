f4c3848fad2453678e05c67caf5f47c7dcdbf630
CodePoet80 <jonathan.m.wise@gmail.com>
2018-10-21 06:50:51 -0400

    fix bash scripts

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -1,1 +1,3 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd $DIR
 palm-package ../ -o ../_bin
--- _scripts/run.sh
+++ _scripts/run.sh
@@ -1,1 +1,3 @@
-palm-run ../
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd $DIR/../
+palm-run "./"
